72badbf0a628d2e8525ecf7e06ddc54c7d58767c
Joe Amditis <jamditis@gmail.com>
2023-03-23 18:52:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,12 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  function filterArticlesBySource(articles, sourceId) {
-    return articles.filter((article) => article.source.id === sourceId);
-  }
-
-  fetchNewsArticles();
-  const rssFeedUrls = [
+  fetchNewsArticles([
   "http://rssfeeds.northjersey.com/northjerseynews",
   "http://rssfeeds.app.com/asburypark/home",
   "https://njbiz.com/feed/?cat=630,231,275,232,216,217,233,276,219,218",
@@ -93,8 +88,80 @@ document.addEventListener("DOMContentLoaded", function () {
   "https://www.70and73.com/",
   "https://www.njspotlightnews.org/news/feed/",
   "https://www.nj.com/arc/outboundfeeds/rss/?outputType=xml",
-  // Add more RSS feed URLs here
-];
+]);
 
-fetchNewsArticles(rssFeedUrls);
-});
+function sortArticlesByDate(articles, descending = true) {
+  return articles.sort((a, b) => {
+    const dateA = new Date(a.pubDate);
+    const dateB = new Date(b.pubDate);
+    return descending ? dateB - dateA : dateA - dateB;
+  });
+}
+
+function displayNewsArticles(articles, container) {
+  container.innerHTML = "";
+
+  articles.forEach((article) => {
+    const articleElement = document.createElement("div");
+    articleElement.classList.add("article");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = article.imageUrl;
+    imgElement.alt = article.title;
+
+    const titleElement = document.createElement("h3");
+    titleElement.textContent = article.title;
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = article.description;
+
+    articleElement.appendChild(imgElement);
+    articleElement.appendChild(titleElement);
+    articleElement.appendChild(descriptionElement);
+    container.appendChild(articleElement);
+  });
+}
+
+function fetchNewsArticles(feedUrls) {
+  Promise.all(feedUrls.map((feedUrl) => fetchRssFeed(feedUrl)))
+    .then((allArticles) => {
+      const articles = allArticles.flat();
+      const sortedArticles = sortArticlesByDate(articles);
+      displayNewsArticles(sortedArticles, newsContainer);
+    })
+    .catch((error) => {
+      console.error("Error fetching news articles:", error);
+    });
+}
+
+function fetchRssFeed(feedUrl) {
+  const proxyUrl = "https://api.allorigins.win/raw?url=";
+  const url = proxyUrl + encodeURIComponent(feedUrl);
+
+  return fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, "application/xml");
+      const items = xml.querySelectorAll("item");
+      const articles = [];
+
+      items.forEach((item) => {
+        const title = item.querySelector("title").textContent;
+        const link = item.querySelector("link").textContent;
+        const description = item.querySelector("description").textContent;
+        const pubDate = item.querySelector("pubDate").textContent;
+        const imageUrl = item.querySelector("media\\:content, content")?.getAttribute("url") || "";
+
+        articles.push({
+          title,
+          link,
+          description,
+          pubDate,
+          imageUrl,
+        });
+      });
+
+      return articles;
+    });
+}
