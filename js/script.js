d41ff423074aff3eebc2689b645aa40a601381eb
Joe Amditis <jamditis@gmail.com>
2023-03-23 18:50:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const apiKey = "c3a1e9c543ac4cdb9162bdc8f3c644d1";
   const newsContainer = document.getElementById("news-stories");
   const trendingList = document.getElementById("trending-list");
   const dateRangeSelect = document.getElementById("trending-date-range");
 
-  function fetchTopHeadlines() {
-    const url = `https://newsapi.org/v2/top-headlines?country=us&q=New%20Jersey&apiKey=${apiKey}`;
+  function fetchRssFeed(feedUrl) {
+    const proxyUrl = "https://api.allorigins.win/raw?url=";
+    const url = proxyUrl + encodeURIComponent(feedUrl);
 
-    fetch(url)
-      .then((response) => response.json())
+    return fetch(url)
+      .then((response) => response.text())
       .then((data) => {
-        const sortedArticles = sortArticlesByDate(data.articles);
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(data, "application/xml");
+        const items = xml.querySelectorAll("item");
+        const articles = [];
+
+        items.forEach((item) => {
+          const title = item.querySelector("title").textContent;
+          const link = item.querySelector("link").textContent;
+          const description = item.querySelector("description").textContent;
+          const pubDate = item.querySelector("pubDate").textContent;
+          const imageUrl = item.querySelector("media\\:content, content")?.getAttribute("url") || "";
+
+          articles.push({
+            title,
+            link,
+            description,
+            pubDate,
+            imageUrl,
+          });
+        });
+
+        return articles;
+      });
+  }
+
+  function fetchNewsArticles(feedUrls) {
+    Promise.all(feedUrls.map((feedUrl) => fetchRssFeed(feedUrl)))
+      .then((allArticles) => {
+        const articles = allArticles.flat();
+        const sortedArticles = sortArticlesByDate(articles);
         displayNewsArticles(sortedArticles, newsContainer);
       })
       .catch((error) => {
@@ -26,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
       articleElement.classList.add("article");
 
       const imgElement = document.createElement("img");
-      imgElement.src = article.urlToImage;
+      imgElement.src = article.imageUrl;
       imgElement.alt = article.title;
 
       const titleElement = document.createElement("h3");
@@ -42,10 +71,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-function sortArticlesByDate(articles, descending = true) {
+  function sortArticlesByDate(articles, descending = true) {
     return articles.sort((a, b) => {
-      const dateA = new Date(a.publishedAt);
-      const dateB = new Date(b.publishedAt);
+      const dateA = new Date(a.pubDate);
+      const dateB = new Date(b.pubDate);
       return descending ? dateB - dateA : dateA - dateB;
     });
   }
@@ -54,5 +83,18 @@ function sortArticlesByDate(articles, descending = true) {
     return articles.filter((article) => article.source.id === sourceId);
   }
 
-  fetchTopHeadlines();
+  fetchNewsArticles();
+  const rssFeedUrls = [
+  "http://rssfeeds.northjersey.com/northjerseynews",
+  "http://rssfeeds.app.com/asburypark/home",
+  "https://njbiz.com/feed/?cat=630,231,275,232,216,217,233,276,219,218",
+  "https://www.roi-nj.com/feed/",
+  "https://pressofatlanticcity.com/search/?q=",
+  "https://www.70and73.com/",
+  "https://www.njspotlightnews.org/news/feed/",
+  "https://www.nj.com/arc/outboundfeeds/rss/?outputType=xml",
+  // Add more RSS feed URLs here
+];
+
+fetchNewsArticles(rssFeedUrls);
 });
